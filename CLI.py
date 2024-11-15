6e3a46278e3cff107330e0f8cf9e566452dc7bab
Anthea Ip <antheaip964@gmail.com>
2024-11-14 19:57:44 -0500

    Python

--- CLI.py
+++ CLI.py
@@ -3,7 +3,16 @@ import subprocess
 import json
 import os
 
+app = FastAPI()
 
+# Set up constants 
+MODEL = "llama3.2"
+OLLAMA_CMD = f"/usr/local/bin/ollama run {MODEL}"
+
+OLLAMA_PROMPT = """
+Given a user query describing an 
+
+"""
 
 
 
